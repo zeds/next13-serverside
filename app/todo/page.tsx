1e311148916ae44653c91d860361d05b6f9ec3a6
Tsutomu Okumura <tom.zed39@gmail.com>
2022-12-21 18:06:28 +0900

    lightgray

---
+++ app/todo/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+
+export default async function Todo() {
+
+	return (
+		<div className={styles.container}>
+			ログインページ
+		</div>
+	)
+}
+
---
+++ components/AddTask.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddTask = () => {
+	return (
+		<div className="inputField">
+			<input type="text" className="inputTite" />
+			<button type="button">追加</button>
+		</div>
+	)
+}
+
+export default AddTask;
